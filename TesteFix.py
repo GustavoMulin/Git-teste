9fb6221652db62b26e1a3f3c4fec4226eb7c5fbe
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-02-09 13:42:54 -0400

    [fix]: corrigir erro na desclaração dos tipos primitivos

--- TesteFix.py
+++ TesteFix.py
@@ -1,5 +1,5 @@
-n1 = input("Dígite um número: ")
-n2 = input("Dígite outro número: ")
+n1 = int(input("Dígite um número: "))
+n2 = int(input("Dígite outro número: "))
 soma = n1 + n2
 print("A soma é {}".format(soma))
 
