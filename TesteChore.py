7a378ef165acab7a39c6470fa7981bd301cbb0e5
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-02-09 20:40:05 -0400

    First commit

---
+++ TesteChore.py
@@ -0,0 +1,5 @@
+# Converso de Real para Dólar
+
+real = float(input('Quanto de dinheiro você tem na carteira? R$'))
+dolar = real / 5.86
+print('Com {:.2f}R$ você comprar {:.2f}US$'.format(real, dolar))
