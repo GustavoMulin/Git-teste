efdecc8ece6b85805ed54321371cf6d6b0fe00af
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-02-09 20:10:31 -0400

    [Test]: Adicionei teste manual para multiplicação

--- TesteTest.py
+++ TesteTest.py
@@ -2,5 +2,11 @@
 a = 3
 b = 4
 multiplicacao = a * b
-print(multiplicacao)
+resultado_esperado = 12
+print("Resultado: {}".format(multiplicacao))
+
+if multiplicacao == resultado_esperado:
+    print("Teste aprovado")
+else:
+    print("Falhou no teste")
 
