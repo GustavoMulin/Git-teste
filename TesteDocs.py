bcfe8f470f431215f0798cf4fc5bdaf0b728654d
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-02-09 20:32:13 -0400

    [Docs]: Adicionar documentação ao cálculo da área e da quantidade de litros

--- TesteDocs.py
+++ TesteDocs.py
@@ -1,7 +1,23 @@
 # Pintandp a parede
+"""
+Este programa calcula a área e a quantidade de litros nescessário para pintar uma parede
+
+Fórmula:
+    área = largura * altura
+"""
+
+# Definição das dimensões 
 larg = float(input('Largura da parede: '))
 alt = float(input('Altura da parede: '))
+
+# Cáculo da área
 area = larg * alt
+
+# Exibe o resultado da área
 print('Sua parede tem a dimensão de {}x{} e sua área é de {}m².'.format(larg, alt, area))
+
+# Cálculo de litros de tinta
 tinta = area / 2
+
+# Exibe quantidade
 print('Para pintar essa parede você precisará de {}L de tintas'.format(tinta))
