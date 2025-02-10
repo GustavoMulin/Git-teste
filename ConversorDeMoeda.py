35de8a3e9715d1f0895c2a73a6a8a589cf82539a
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-02-09 20:44:55 -0400

    [Chore]: Renomear arquivo TesteChore.py para ConversorDeMoeda.py

--- ConversorDeMoeda.py
+++ ConversorDeMoeda.py
@@ -1,5 +1,5 @@
 # Converso de Real para Dólar
 
 real = float(input('Quanto de dinheiro você tem na carteira? R$'))
-dolar = real / 5.86
+dolar = real / 5.81
 print('Com {:.2f}R$ você comprar {:.2f}US$'.format(real, dolar))
